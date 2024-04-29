4b97730f815acf136c345f0d62d6634afa3ffa27
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-04-30 00:06:39 +0200

    updateStationLogo.js

--- StationLogo/updateStationLogo.js
+++ StationLogo/updateStationLogo.js
@@ -1,12 +1,12 @@
 //////////////////////////////////////////////////////////////////////////////////////
 ///                                                                                ///
-///  STATION LOGO INSERT SCRIPT FOR FM-DX-WEBSERVER (V2.2)			   ///
+///  STATION LOGO INSERT SCRIPT FOR FM-DX-WEBSERVER (V2.3)			   ///
 ///                                                                                /// 
 ///  Thanks to Ivan_FL, Adam W, mc_popa & noobish for the ideas, code and design!  ///
 ///                                                                                ///
 ///  New Logo Files (png/svg) and Feedback are welcome!                            ///
 ///  73! Highpoint                                                                 ///
-///                                                          last update: 26.04.24 ///
+///                                                          last update: 29.04.24 ///
 //////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -16,21 +16,21 @@
 var parentDiv = document.querySelector('.panel-75.flex-container.no-bg');
 
 // Create the new HTML code
-var DesktopHTML = `
-    <div class="panel-10 no-bg h-100 m-0 m-right-20 hide-phone" style="width: 100px;margin-right: 20px !important;">
+
+const DesktopHTML = `
+    <div class="panel-10 no-bg h-50 m-0 m-right-20 hide-phone" style="width: 100px;height: 100%;margin-right: 20px !important;">
         <button class="playbutton" aria-label="Play / Stop Button"><i class="fa-solid fa-play fa-lg"></i></button>
     </div>
     <div class="panel-75 m-0 m-right-20 hover-brighten flex-center tooltip" id="ps-container" style="height: 90px;margin-right: 20px!important" data-tooltip="Clicking on the RDS PS will copy the RDS info into the clipboard.">
         <span class="text-big" id="data-ps"></span>
-    </div>					
-    <div class="panel-30 m-0 hide-phone" style="width: 35%;">
+    </div>
+    <div class="panel-30 m-0 hide-phone tooltip" style="width: 35%" data-tooltip="">
         <div id="logo-container-desktop" style="width: auto; height: 60px; display: flex; justify-content: center; align-items: center; margin: auto;">                 
-            <img id="station-logo" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAAtJREFUGFdjYAACAAAFAAGq1chRAAAAAElFTkSuQmCC" alt="station-logo-desktop" style="max-width: 140px; padding: 1px 2px; max-height: 100%; margin-top: 30px; display: block;">	
+            <img id="station-logo" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAAtJREFUGFdjYAACAAAFAAGq1chRAAAAAElFTkSuQmCC" alt="station-logo-desktop" style="max-width: 140px; padding: 1px 2px; max-height: 100%; margin-top: 30px; display: block; cursor: pointer;">	
         </div>
     </div>
 `;
 
-// Set the new HTML as the content of the parent <div> element
 parentDiv.innerHTML = DesktopHTML;
 
 //////////////// Inject Logo Code for Mobile Devices ////////////////////////
@@ -54,81 +54,81 @@ var MobileHTML = `
 // Replace the HTML content of the <div> element with the new HTML code
 flagsContainerPhone.innerHTML = MobileHTML;
 
-//////////////// Display the Logo  ////////////////////////
+// Display the Logo //
 
-var serverpath = 'https://tef.noobish.eu/logos/'; // Server Path for official Logo Collection
-var localpath = '/logos/'; // Webdirectory Path for local Logo Collection
-var defaultServerPath = serverpath + 'default-logo.png'; // Display Standard Logo
-// var defaultServerPath = serverpath + 'empty-logo.png'; // No Display
+const serverpath = 'https://tef.noobish.eu/logos/';
+const localpath = '/logos/';
+const defaultServerPath = serverpath + 'default-logo.png';
 
-// Refresh the page after a delay of 1 second
+if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+    var logoImage = $('#station-logo-phone');
+} else {
+    var logoImage = $('#station-logo');
+}
+
 setInterval(CheckPI, 1000);
 
 function CheckPI() {
-    var piCodeContainer = $('#data-pi');
-    var piCode = piCodeContainer.text().trim().replace('?', '').replace('??', '');
-    var piCode = piCode.toUpperCase();
+    const piCodeContainer = $('#data-pi');
+    const piCode = piCodeContainer.text().trim().replace(/[?]{1,2}/g, '').toUpperCase();
+    const sender = $('#data-station-name').text().trim();
+    const tooltipContainer = $('.panel-30');
 
-    if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
-        var logoImage = $('#station-logo-phone');
-    } else {
-        var logoImage = $('#station-logo');
-    }
+    tooltipContainer.attr('data-tooltip', `Google Search the right or missing Logo, save as *${piCode}*.svg or *${piCode}*.png and send this with country info to highpoint2000@googlemail.com`);
 
-    if (piCode === '') {
-        logoImage.attr('src', defaultServerPath);
-        logoImage.attr('alt', 'Default Logo');
-        window.piCode = piCode;
+    if (piCode === '' || piCode.includes('?')) {
+        tooltipContainer.trigger('mouseleave').css('background-color', '');
+        updateStationLogo(defaultServerPath, piCode, sender);
     } else {
-        updateStationLogo(piCode, logoImage);
+        updateStationLogo(logoImage.attr('src'), piCode, sender);
     }
 }
 
-function updateStationLogo(piCode, logoImage) {
-    var ituCode = $('#data-station-itu').text().trim();
+function updateStationLogo(currentImagePath, piCode, sender) {
+    const ituCode = $('#data-station-itu').text().trim();
+    const tooltipContainer = $('.panel-30');
+
     if (piCode !== window.piCode || ituCode !== window.ituCode) {
         window.piCode = piCode;
         window.ituCode = ituCode;
 
-        var paths = [
-            serverpath + ituCode + '/' + piCode + '.gif',
-            serverpath + ituCode + '/' + piCode + '.svg',
-            serverpath + ituCode + '/' + piCode + '.png',
-            localpath + piCode + '.gif',
-            localpath + piCode + '.svg',
-            localpath + piCode + '.png'
+        const paths = [
+            `${serverpath}${ituCode}/${piCode}.gif`,
+            `${serverpath}${ituCode}/${piCode}.svg`,
+            `${serverpath}${ituCode}/${piCode}.png`,
+            `${localpath}${piCode}.gif`,
+            `${localpath}${piCode}.svg`,
+            `${localpath}${piCode}.png`
         ];
 
-        // Function to set logo and alt
-        function setLogo(src, alt) {
-            logoImage.attr('src', src);
-            logoImage.attr('alt', alt);
-            logoImage.css('display', 'block');
-        }
-
-        // Check if the specific PI code image exists
-        var found = false;
-        var i = 0;
+        let found = false;
+        let i = 0;
 
         function checkNext() {
             if (found || i >= paths.length) {
                 if (!found) {
-                    setLogo(defaultServerPath, 'Default Logo');
+                    logoImage.attr('src', defaultServerPath).attr('alt', 'Default Logo');
+                    LogoSearch(piCode, sender); // Call LogoSearch() if no logo is found
                 }
                 return;
             }
-            $.ajax({
-                url: paths[i],
-                type: 'HEAD',
-                success: function() {
-                    setLogo(paths[i], 'Logo for station ' + piCode);
+            const xhr = new XMLHttpRequest();
+            xhr.open('HEAD', paths[i], true);
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    logoImage.attr('src', paths[i]).attr('alt', `Logo for station ${piCode}`).css('display', 'block');
                     found = true;
-                },
-                error: function() {
+                    LogoSearch(piCode, sender); // Call LogoSearch() if a logo is found
+                } else {
                     i++;
                     checkNext();
                 }
-            });
+            };
+            xhr.onerror = function () { // Handling error cases
+                i++;
+                checkNext();
+            };
+            xhr.send();
         }
 
         checkNext();
@@ -136,4 +136,29 @@ function updateStationLogo(piCode, logoImage) {
 }
 
 
+var previousSender = ''; // Global variable to store the previous sender
 
+function LogoSearch(piCode) {
+    const tooltipContainer = $('.panel-30');
+    tooltipContainer.css('background-color', '').off('click').css('cursor', 'auto');
+
+    function addClickListener(currentSender) {
+        tooltipContainer.css('background-color', 'var(--color-2)').on('click', () => {
+            const ituCode = $('#data-station-itu').text().trim();
+            const searchQuery = currentSender + ' SVG PNG Radio&tbs=sbd:1&udm=2';
+            window.open('https://www.google.com/search?q=' + searchQuery, '_blank');
+        });
+    }
+
+    function checkSender() {
+        const currentSender = $('#data-station-name').text().trim();
+        if (currentSender && currentSender !== previousSender) {
+            addClickListener(currentSender);
+            previousSender = currentSender;
+        } else {
+            setTimeout(checkSender, 1000);
+        }
+    }
+
+    checkSender();
+}
