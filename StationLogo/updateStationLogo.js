e5c8be3246a9b6e985442f6ef29b5f2c552a5c0a
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-05-19 07:47:02 +0200

    Update updateStationLogo.js

--- StationLogo/updateStationLogo.js
+++ StationLogo/updateStationLogo.js
@@ -6,7 +6,7 @@
 ///                                                                                ///
 ///  New Logo Files (png/svg) and Feedback are welcome!                            ///
 ///  73! Highpoint                                                                 ///
-///                                                          last update: 17.05.24 ///
+///                                                          last update: 19.05.24 ///
 //////////////////////////////////////////////////////////////////////////////////////
 
 
